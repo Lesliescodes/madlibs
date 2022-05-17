5211ad9721d7e980d14b0a4814897372eb372c8a
Leslie Allen <lesliecodes@gmail.com>
2022-05-16 23:11:36 -0600

    quick little project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # madlibs
+
+Madlibs a fun word puzzile. Simple beginner project. Try it for yourself!
+
+## Tech
+
+madlib uses python and Visual studio code
+
+## Installation
+
+madlib requires python to run
+
+```sh
+py app.py
+```
+
+>
---
+++ app.py
@@ -0,0 +1,10 @@
+adj = input('Adjective')
+verb1 = input('Verb: ')
+verb2 = input('verb: ')
+famous_person = input('famous_person:')
+
+
+madlib = f"Trying new foods is always so {adj}! It makes me so happy being in a new city \
+I love to {verb1}. Stay happy and {verb2} like you are {famous_person}!"
+
+print(madlib)
